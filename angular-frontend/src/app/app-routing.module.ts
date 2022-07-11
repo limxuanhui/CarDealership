2aad839486b294fc097664de31a40de99c9ff261
Lim Xuan Hui <limxuanhui1717@gmail.com>
2022-07-11 15:52:44 +0800

    Added routes to routing module

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { InventorySearchPageComponent } from './pages/inventory-search-page/inventory-search-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "inventory/new",
+    component: InventorySearchPageComponent
+  },
+  {
+    path: "inventory/used",
+    component: InventorySearchPageComponent
+  },
+  {
+    path: "home",
+    component: HomePageComponent
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
